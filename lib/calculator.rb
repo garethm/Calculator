58ef9d978229d822c4503f1c0dcbb31848489618
Gareth Marshall <garethm@acm.org>
2011-02-20 21:36:02 +1000

    Requirement 4 test passes

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,16 @@
 module Calculator
   class Calculator
     def add numbers
-      numbers.split(/(,|\n)/).reduce(0) do |sum, number|
+      if numbers.start_with? '//'
+        delimiter_string = numbers.split(/\n/)[0][2..-1]
+        options = delimiter_string.chars.reduce([]) do |list, item|
+          list << Regexp.escape(item)
+        end
+        delimiters = Regexp.union(options)
+      else
+        delimiters = /(,|\n)/
+      end
+      numbers.split(delimiters).reduce(0) do |sum, number|
         sum + number.to_i
       end
     end
