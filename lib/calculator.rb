90d95581a82e1a22d4886a90ecd8535dab176157
Gareth Marshall <garethm@acm.org>
2011-02-20 22:12:15 +1000

    Broke up regexp parsing a bit more

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -6,17 +6,24 @@ module Calculator
       not @input.start_with? '//'
     end
 
-    def get_parsed_delimiters
-      delimiter_string = @input.split(/\n/)[0][2..-1]
+    def delimiter_line
+      @input.split(/\n/)[0]
+    end
+
+    def get_delimiter_regexp delimiter_string
       options = delimiter_string.chars.reduce([]) do |list, item|
         list << Regexp.escape(item)
       end
       Regexp.union(options)
     end
 
+    def get_parsed_delimiters
+      get_delimiter_regexp delimiter_line[2..-1]
+    end
+
     def get_delimiters
       return DEFAULT_DELIMITER if use_default_delimiter?
-      return get_parsed_delimiters
+      get_parsed_delimiters
     end
 
     def add input
