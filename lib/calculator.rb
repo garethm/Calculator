976eb4cce8afccefc3c9698d3b480be509c7a972
Gareth Marshall <garethm@acm.org>
2011-02-20 21:40:47 +1000

    Refactored out some of the delimiter code

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,21 @@
 module Calculator
   class Calculator
+    DEFAULT_DELIMITER = /(,|\n)/
+
+    def use_default_delimiter? numbers
+      not numbers.start_with? '//'
+    end
+
+    def get_delimiters numbers
+      return DEFAULT_DELIMITER if use_default_delimiter?
+
+      delimiter_string = numbers.split(/\n/)[0][2..-1]
+      options = delimiter_string.chars.reduce([]) do |list, item|
+        list << Regexp.escape(item)
+      end
+      Regexp.union(options)
+    end
+
     def add numbers
       if numbers.start_with? '//'
         delimiter_string = numbers.split(/\n/)[0][2..-1]
