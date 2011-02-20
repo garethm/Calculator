0bf32bb984d5f4ed3a3745d1fc82070aa7a2c184
Gareth Marshall <garethm@acm.org>
2011-02-20 17:30:45 +1000

    Added text from instructions as rspec items

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,9 @@
+module Calculator
+  describe Calculator do
+    describe "add" do
+      it "accepts 0 numbers and returns 0"
+      it "accepts 1 number and returns the same number"
+      it "accepts 2 numbers and returns the sum"
+    end
+  end
+end
