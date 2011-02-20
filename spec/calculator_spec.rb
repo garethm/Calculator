03dba82b1bd9957f2a352800e5b2aab3f59badf9
Gareth Marshall <garethm@acm.org>
2011-02-20 21:08:29 +1000

    Refactored test to pull out construction of calculator instance

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 module Calculator
   describe Calculator do
     describe "add" do
+      before(:each) do
+        @calculator = Calculator.new
+      end
       it "accepts 0 numbers and returns 0" do
-        calculator = Calculator.new
-        result = calculator.add("")
+        result = @calculator.add("")
         result.should equal 0
       end
       it "accepts 1 number and returns the same number" do
-        calculator = Calculator.new
-        result = calculator.add("1")
+        result = @calculator.add("1")
         result.should equal 1
       end
       it "accepts 2 numbers and returns the sum" do
-        calculator = Calculator.new
-        result = calculator.add("1,2")
+        result = @calculator.add("1,2")
         result.should equal 3
       end
     end
