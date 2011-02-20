700a37cadde66df5b254e868f00e224c407e5201
Gareth Marshall <garethm@acm.org>
2011-02-20 23:06:56 +1000

    Further cleanups

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,19 +5,25 @@ module Calculator
     def initialize input
       @input = input
       @delimiters = get_delimiters
-      if use_default_delimiter?
-        @data = @input
-      else
-        @data = @input[@input.index("\n")+1..-1]
-      end
+      @data = get_data
+    end
+
+    def get_delimiters
+      return DEFAULT_DELIMITER if use_default_delimiter?
+      get_parsed_delimiters
+    end
+
+    def get_data
+      return @input if use_default_delimiter?
+      @input[@input.index("\n")+1..-1]
     end
 
     def use_default_delimiter?
       not @input.start_with? '//'
     end
 
-    def delimiter_line
-      @input.split(/\n/)[0]
+    def get_parsed_delimiters
+      get_delimiter_regexp delimiter_line[2..-1]
     end
 
     def get_delimiter_regexp delimiter_string
@@ -27,20 +33,13 @@ module Calculator
       Regexp.union(options)
     end
 
-    def get_parsed_delimiters
-      get_delimiter_regexp delimiter_line[2..-1]
-    end
-
-    def get_delimiters
-      return DEFAULT_DELIMITER if use_default_delimiter?
-      get_parsed_delimiters
+    def delimiter_line
+      @input.split(/\n/)[0]
     end
 
     def numbers
       list = @data.split @delimiters
-      list.map do |number|
-        number.to_i
-      end
+      list.map { |number| number.to_i }
     end
   end
 end
