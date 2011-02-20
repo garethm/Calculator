3b8dc313a9a317a491d9c4e10c3b1f3871fe5eee
Gareth Marshall <garethm@acm.org>
2011-02-20 21:52:29 +1000

    Refactored to use field for input

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,31 +2,24 @@ module Calculator
   class Calculator
     DEFAULT_DELIMITER = /(,|\n)/
 
-    def use_default_delimiter? numbers
-      not numbers.start_with? '//'
+    def use_default_delimiter?
+      not @input.start_with? '//'
     end
 
-    def get_delimiters numbers
+    def get_delimiters
       return DEFAULT_DELIMITER if use_default_delimiter?
 
-      delimiter_string = numbers.split(/\n/)[0][2..-1]
+      delimiter_string = @input.split(/\n/)[0][2..-1]
       options = delimiter_string.chars.reduce([]) do |list, item|
         list << Regexp.escape(item)
       end
       Regexp.union(options)
     end
 
-    def add numbers
-      if numbers.start_with? '//'
-        delimiter_string = numbers.split(/\n/)[0][2..-1]
-        options = delimiter_string.chars.reduce([]) do |list, item|
-          list << Regexp.escape(item)
-        end
-        delimiters = Regexp.union(options)
-      else
-        delimiters = /(,|\n)/
-      end
-      numbers.split(delimiters).reduce(0) do |sum, number|
+    def add input
+      @input = input
+
+      @input.split(get_delimiters).reduce(0) do |sum, number|
         sum + number.to_i
       end
     end
